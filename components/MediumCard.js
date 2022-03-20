3a839065cdf50c160cc7c6d91a8c082d79b6d67e
Saurabh Kumar <saukumar@nsf.org>
2022-03-20 18:46:33 +0530

    Search page implemented

--- components/MediumCard.js
+++ components/MediumCard.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Image from 'next/image';
 
 function MediumCard({ img, title }) {
-    console.log(title)
   return (
     <div className='cursor-pointer hover:scale-105 transform transition duration-300 ease-out'>
         <div className="relative h-80 w-80">
--- components/SmallCard.js
+++ components/SmallCard.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Image from 'next/image';
 
 function SmallCard({ img, location, distance }) {
-    console.log(img)
   return (
     <div className="flex items-center m-2 mt-5 space-x-4 rounded-xl cursor-pointer hover:bg-gray-100 hover:scale-105 transition transform duration-200 ease-out">
         {/* left */}
